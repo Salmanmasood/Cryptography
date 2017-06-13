1a63dc3ecb63af72d3762e0171e78a59cb1a8a5d
user <salmanmasood24@yahoo.com>
2017-06-13 07:37:56 -0700

    added file

---
+++ Asymmetric/ConsoleApplication1/ConsoleApplication1/AsymmetricEncryptionDemo .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class AsymmetricEncryptionDemo 
+    {
+ public  static byte[] EncryptData(string plainText, RSAParameters rsaParameters)
+        { 
+     
+            byte[] plainTextArray = new UnicodeEncoding().GetBytes(plainText); 
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSA.ImportParameters(rsaParameters);
+            byte[] encryptedData = RSA.Encrypt(plainTextArray, true);
+            return encryptedData; 
+        }
+
+
+ public static byte[] DecryptData(byte[] encryptedData, RSAParameters rsaParameters)
+        {
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(); 
+            RSA.ImportParameters(rsaParameters);
+            byte[] decryptedData = RSA.Decrypt(encryptedData, true);
+            return decryptedData;
+        }
+    
+    }
+}
---
+++ Asymmetric/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter text to encrypt:");
+            String inputText = Console.ReadLine();
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(); 
+            RSAParameters RSAParam=RSA.ExportParameters(false); 
+            byte[] encryptedData =AsymmetricEncryptionDemo.EncryptData(inputText, RSAParam);
+            string encryptedString = Encoding.Default.GetString(encryptedData); 
+            Console.WriteLine("\nEncrypted data \n{0}",encryptedString);
+            byte[] decryptedData = AsymmetricEncryptionDemo.DecryptData(encryptedData, RSA.ExportParameters(true)); 
+            String decryptedString = new UnicodeEncoding().GetString(decryptedData);
+            Console.WriteLine("\nDecrypted data \n{0}", decryptedString);
+
+            Console.ReadLine();
+        }
+    }
+}
